0399d6eab299f1456fc2966b8a54b9f4940ced12
mvivy3 <meredithyen@usf.edu>
2025-02-09 22:06:31 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -18,3 +18,21 @@ const calculateSalesTax = function(amount, taxRate) {
 };
 console.log("Sales Tax: $", calculateSalesTax(100, 0.07)); // Sales Tax: $7
 console.log("Sales Tax: $", calculateSalesTax(500, 0.1)); // Sales Tax: $50
+
+
+// Task 3: Employee Bonus Calculation
+
+// Write an arrow function to determine a bonus
+const calculateBonus = (salary, performanceRating) => {
+let bonus;
+    if (performanceRating === "Excellent"){
+        bonus = salary * .20;
+    } else if (performanceRating === "Good") {
+        bonus = salary * .10;
+    } else if (performanceRating === "Average") {
+        bonus = salary * .05;
+    }
+    return bonus;
+}
+console.log("Bonus: $", calculateBonus(5000, "Excellent")); // Log the bonus, Bonus: $1000
+console.log("Bonus: $", calculateBonus(7000, "Good")); // Bonus: $7000
